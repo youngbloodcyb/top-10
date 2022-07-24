3023ebfd7fc9291decb988daadd3bd0c3324e06c
youngbloodcyb <cameron.youngblood@gmail.com>
2022-07-23 21:54:08 -0600

    move client and account init code to stores

--- src/routes/success.svelte
+++ src/routes/success.svelte
@@ -1,30 +1,6 @@
 <script>
-    import { Client, Account } from "appwrite";
-
-    const client = new Client();
-
-    const account = new Account(client);
-
-    client
-        .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // Your API Endpoint
-        .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID) // Your project ID
-    ;
-
-    let name = "";
-
-    const getAccount = () => { account.get()
-        .then(function (response) {
-            console.log(response); // Success
-            name = response.name;
-            name = name;
-        }, function (error) {
-            console.log(error); // Failure
-            return error;
-        });
-    }
-
-    getAccount();
+    import { user } from "../stores/getUser";
 </script>
 
 <h1 class="text-6xl">Success!</h1>
-<h1>Welcome, {name}</h1>
+<h1>Welcome, {$user.name}</h1>
